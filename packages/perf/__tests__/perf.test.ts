09943014a8127ed956cc1b7ec0efe293bcde9263
Mike Hardy <github@mikehardy.net>
2025-02-09 13:14:11 -0500

    refactor(test): include all namespaced API tests in namespace sub-suites
    this will let us treat all namespaced API usages as a group in the sub-suites

--- packages/perf/__tests__/perf.test.ts
+++ packages/perf/__tests__/perf.test.ts
@@ -12,27 +12,7 @@ import perf, {
 
 describe('Performance Monitoring', function () {
   describe('namespace', function () {
-    it('accessible from firebase.app()', function () {
-      const app = firebase.app();
-      expect(app.perf).toBeDefined();
-      expect(app.perf().app).toEqual(app);
-    });
-  });
-
-  describe('setPerformanceCollectionEnabled', function () {
-    it('errors if not boolean', function () {
-      expect(async () => {
-        // @ts-ignore
-        await perf().setPerformanceCollectionEnabled();
-      }).rejects.toThrow('must be a boolean');
-    });
-  });
-
-  describe('newTrace()', function () {
-    it('returns an instance of Trace', function () {
-      const trace = perf().newTrace('invertase');
-      expect(trace.constructor.name).toEqual('Trace');
-
+    beforeAll(async function () {
       // @ts-ignore
       expect(trace._identifier).toEqual('invertase');
     });
@@ -61,70 +41,70 @@ describe('Performance Monitoring', function () {
         return Promise.resolve();
       }
     });
-  });
 
-  describe('newHttpMetric()', function () {
-    it('returns an instance of HttpMetric', async function () {
-      const metric = perf().newHttpMetric('https://invertase.io', 'GET');
-      expect(metric.constructor.name).toEqual('HttpMetric');
+    describe('newHttpMetric()', function () {
+      it('returns an instance of HttpMetric', async function () {
+        const metric = perf().newHttpMetric('https://invertase.io', 'GET');
+        expect(metric.constructor.name).toEqual('HttpMetric');
 
-      // @ts-ignore
-      expect(metric._url).toEqual('https://invertase.io');
+        // @ts-ignore
+        expect(metric._url).toEqual('https://invertase.io');
 
-      // @ts-ignore
-      expect(metric._httpMethod).toEqual('GET');
+        // @ts-ignore
+        expect(metric._httpMethod).toEqual('GET');
+      });
+
+      it('errors if url not a string', async function () {
+        try {
+          // @ts-ignore
+          perf().newHttpMetric(1337, 7331);
+          return Promise.reject(new Error('Did not throw'));
+        } catch (e: any) {
+          expect(e.message).toEqual("firebase.perf().newHttpMetric(*, _) 'url' must be a string.");
+          return Promise.resolve();
+        }
+      });
+
+      it('errors if httpMethod not a string', async function () {
+        try {
+          // @ts-ignore
+          perf().newHttpMetric('https://invertase.io', 1337);
+          return Promise.reject(new Error('Did not throw'));
+        } catch (e: any) {
+          expect(e.message).toEqual(
+            "firebase.perf().newHttpMetric(_, *) 'httpMethod' must be one of CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE.",
+          );
+          return Promise.resolve();
+        }
+      });
+
+      it('errors if httpMethod not a valid type', async function () {
+        try {
+          // @ts-ignore
+          perf().newHttpMetric('https://invertase.io', 'FIRE');
+          return Promise.reject(new Error('Did not throw'));
+        } catch (e: any) {
+          expect(e.message).toEqual(
+            "firebase.perf().newHttpMetric(_, *) 'httpMethod' must be one of CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE.",
+          );
+          return Promise.resolve();
+        }
+      });
     });
 
-    it('errors if url not a string', async function () {
-      try {
-        // @ts-ignore
-        perf().newHttpMetric(1337, 7331);
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e: any) {
-        expect(e.message).toEqual("firebase.perf().newHttpMetric(*, _) 'url' must be a string.");
-        return Promise.resolve();
-      }
-    });
-
-    it('errors if httpMethod not a string', async function () {
-      try {
-        // @ts-ignore
-        perf().newHttpMetric('https://invertase.io', 1337);
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e: any) {
-        expect(e.message).toEqual(
-          "firebase.perf().newHttpMetric(_, *) 'httpMethod' must be one of CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE.",
-        );
-        return Promise.resolve();
-      }
-    });
-
-    it('errors if httpMethod not a valid type', async function () {
-      try {
-        // @ts-ignore
-        perf().newHttpMetric('https://invertase.io', 'FIRE');
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e: any) {
-        expect(e.message).toEqual(
-          "firebase.perf().newHttpMetric(_, *) 'httpMethod' must be one of CONNECT, DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT, TRACE.",
-        );
-        return Promise.resolve();
-      }
-    });
-  });
-
-  describe('setPerformanceCollectionEnabled()', function () {
-    it('errors if not boolean', async function () {
-      try {
-        // @ts-ignore
-        await firebase.perf().setPerformanceCollectionEnabled();
-        return Promise.reject(new Error('Did not throw'));
-      } catch (e: any) {
-        expect(e.message).toEqual(
-          "firebase.perf().setPerformanceCollectionEnabled(*) 'enabled' must be a boolean.",
-        );
-        return Promise.resolve();
-      }
+    describe('setPerformanceCollectionEnabled()', function () {
+      it('errors if not boolean', async function () {
+        try {
+          // @ts-ignore
+          await firebase.perf().setPerformanceCollectionEnabled();
+          return Promise.reject(new Error('Did not throw'));
+        } catch (e: any) {
+          expect(e.message).toEqual(
+            "firebase.perf().setPerformanceCollectionEnabled(*) 'enabled' must be a boolean.",
+          );
+          return Promise.resolve();
+        }
+      });
     });
   });
 
